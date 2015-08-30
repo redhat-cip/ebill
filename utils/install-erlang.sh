ffde6935e477d695fcf49fc62cb13e6d6a52b72d
Sebastien Badia <sbadia@redhat.com>
2015-08-30 22:49:09 +0200

    utils: Add erlang and ebill utils script (packaging)

---
+++ utils/install-erlang.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd ~
+/usr/bin/curl -s -O https://raw.githubusercontent.com/spawngrid/kerl/master/kerl
+mkdir ~/bin
+
+PATH=$PATH:~/bin/
+mv kerl ~/bin
+chmod +x ~/bin/kerl
+kerl update releases
+kerl build 17.5 17.5
+kerl install 17.5 ~/.kerl/installs
